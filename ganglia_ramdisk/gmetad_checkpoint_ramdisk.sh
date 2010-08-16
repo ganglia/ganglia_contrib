1916dfe2edc41dc7f803d828bceab03c779796f2
Ben Hartshorne <green@pdp49.lindenlab.com>
2010-08-17 06:15:50 +0800

    importing ganglia ramdisk setup from http://ben.hartshorne.net/

---
+++ ganglia_ramdisk/gmetad_checkpoint_ramdisk.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# clean up old backups
+#find /var/lib/ganglia/ -name "rrds.bak_*" -mtime +7 -prune -exec rm -rf {} \;
+# lets test this with an ls, shall we?  :)
+#find /var/lib/ganglia/ -name "rrds.bak_*" -mtime +7 -prune -exec ls -ld {} \; -exec rm -rf {} \;
+# ok, the test passed and it looks good.  Delete silently.
+#find /var/lib/ganglia/ -name "rrds.bak_*" -mtime +7 -prune -exec rm -rf {} \;
+
+# find was taking too much disk effort.  Switched to perl and rm.
+# Note - if this machine is off across one of the cron times, it will
+# never delete the directories from 8 days previous.  Check every now and again.
+olddate=`perl -MDate::Manip -e 'print &UnixDate(&ParseDate("8 days ago"), "%Y%m%d.%H");'`
+/bin/rm -rf /var/lib/ganglia/rrds.bak_${olddate}????
+
+
+mydate=`/bin/date +%Y%m%d.%H%M%S`
+nice -n 5 /bin/cp -a --no-preserve=timestamps /mnt/ram0/rrds /var/lib/ganglia/rrds.bak_${mydate}
