cfd107411ff0dcb6a45f652bd72adb9e4c41a427
Vladimir Vuksan <vlists@veus.hr>
2010-09-29 10:29:55 -0400

    Graphite integration

---
+++ graphite_integration/README.md
@@ -0,0 +1,3 @@
+More details on Ganglia Graphite integration please read.
+
+http://vuksan.com/blog/2010/09/29/integrating-graphite-with-ganglia/
---
+++ graphite_integration/ganglia_graphite.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/ruby
+require "rexml/document"
+require 'socket'
+
+ganglia_hostname = 'localhost'
+ganglia_port = 8649
+graphite_host = "localhost"
+graphite_port = "2003"
+
+begin
+  # Open up a socket to gmond
+  file = TCPSocket.open(ganglia_hostname, ganglia_port)
+  # Open up a socke to graphite
+  graphite = TCPSocket.open(graphite_host, graphite_port)
+  # We need current time stamp in UNIX time 
+  now = Time.now.to_i
+  # Parse the XML we got from gmond
+  doc = REXML::Document.new file
+  doc.elements.each("GANGLIA_XML/CLUSTER/HOST") { |element|
+    # Set metric prefix to the host name. Graphite uses dots to separate subtrees
+    # therefore we have to change dots in hostnames to _
+    metric_prefix=element.attributes["NAME"].gsub(".", "_")
+    element.elements.each("METRIC") { |metric|
+      if metric.attributes["TYPE"] != "string"                     
+        graphite.puts "#{metric_prefix}.#{metric.attributes["NAME"]} #{metric.attributes["VAL"]} #{now}\n"
+      end
+    }
+  }
+  graphite.close()
+  file.close()
+rescue
+end
